4858059c89dc64b5d0a7663a33019c55610fa93a
GITSangWoo <dogcenta@gmail.com>
2024-08-08 11:25:24 +0900

    first commit

---
+++ dags/pyspark_movie.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+from pprint import pprint
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import (
+    ExternalPythonOperator,
+    PythonOperator,
+    PythonVirtualenvOperator,
+    BranchPythonOperator,
+    is_venv_installed,
+)
+
+with DAG(
+        'extract1',
+    default_args={
+        'depends_on_past': False,
+        'retries': 1,
+        'retry_delay': timedelta(seconds=3)
+    },
+    description='movie DAG',
+    schedule="10 2 * * *",
+    start_date=datetime(2023, 1, 1),
+    end_date=datetime(2023, 5, 1),
+    catchup=True,
+    tags=['api', 'movie', 'amt'],
+) as dag:
+
